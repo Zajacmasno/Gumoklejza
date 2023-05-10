d00f5665c3b47bf124b7593505ee15afc9b5f31f
Zając <76213206+Zajacmasno@users.noreply.github.com>
2023-05-10 22:14:26 +0200

    szkielet headera

--- gumeklejza-app/src/App.js
+++ gumeklejza-app/src/App.js
@@ -4,8 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
+      <header>
+        <nav>
+          <div id="logo">
+            <h1>LOGO</h1>
+          </div>
+          <div id="PCA">
+            Products Contact About
+          </div>
+          <div id="shopAcc">
+            Shop Account
+          </div>
+        </nav>
+        <div className="dontPanic">
+          <p>Don't Panic,<br></br> it's Organic</p>
+          <button id="makeOrder">Make Order</button>
+        </div>
       </header>
     </div>
   );
