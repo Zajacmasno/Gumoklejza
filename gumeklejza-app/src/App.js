f100a74d53f2edbb76f18857203b2bf458929e44
Zając <76213206+Zajacmasno@users.noreply.github.com>
2023-05-11 15:50:56 +0200

    dawhjd

--- gumeklejza-app/src/App.js
+++ gumeklejza-app/src/App.js
@@ -22,13 +22,11 @@ function App() {
             <button id="makeOrder">Make Order</button>
           </div>
         </header>
-        <div className='air air1'></div>
-        <div className='air air2'></div>
-        <div className='air air3'></div>
-        <div className='air air4'></div>
+        <div className='fala fala1'></div>
+        <div className='fala fala2'></div>
+        <div className='fala fala3'></div>
+        <div className='fala fala4'></div>
       </section>
-
-
       <div className='main'>
         <div className='mainTitle'>
           <h1>
