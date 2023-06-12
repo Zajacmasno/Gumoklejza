3366ee1bc4ce1d1e9bec255a0bc76cbd9fbbcf5d
jsepiol <kuba.sepiol@gmail.com>
2023-06-12 17:54:30 +0200

    dadsa

--- gumeklejza-app/src/pages/sklep.js
+++ gumeklejza-app/src/pages/sklep.js
@@ -5,17 +5,18 @@ import './pages.css';
 function sklep() {
     return (
         <div>
-            <div className="top"><h1>Products</h1></div>
+            <div className="top"><a className="products">Products</a> Main Koszyk</div>
+            
             <div className="main">
                 <div className="row1">
-                    <div className="ciul"> </div>
-                    <div className="ciul"> </div>
-                    <div className="ciul"> </div>
+                    <div className="ciul">Sour Diesel 21$ </div>
+                    <div className="ciul">White Widow 37$</div>
+                    <div className="ciul">Venom OG  25$</div>
                 </div>
                 <div className="row2">
-                <div className="ciul"> </div>
-                    <div className="ciul"> </div>
-                    <div className="ciul"> </div>
+                <div className="ciul">Obama 14$</div>
+                    <div className="ciul">Cheese 15$</div>
+                    <div className="ciul">Cali Gold 19$</div>
                 </div>
             </div>
             <div className="footer"></div>
